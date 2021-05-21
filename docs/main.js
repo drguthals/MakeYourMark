f826a399af317779fd046245fefd6e85242026f2
Sarah Guthals <sarah@guthals.com>
2021-05-21 07:49:14 -0700

    Trying a txt file

--- docs/main.js
+++ docs/main.js
@@ -1,15 +1,27 @@
-let data = fetch("./initial-constellations.json")
-.then(response => {
-   return response.json();
-});
+// Requiring fs module in which
+// readFile function is defined.
+const fs = require('fs')
+  
+// Reading data in utf-8 format
+// which is a type of character set.
+// Instead of 'utf-8' it can be 
+// other character set also like 'ascii'
+fs.readFile('initial-constellations.txt', 'utf-8', (err, data) => {
+    if (err) throw err;
 
-let div = document.getElementById('universe');
+    console.log(data);
+/*  
+    let div = document.getElementById('universe');
 
-for (var i = 0; i < data.length; i++) {
-	div.insertAdjacentHTML(
-		'beforeend',
-        '<iframe style="position:absolute;top:0;left:0;width:10%;height:10%;" src="' +
-        data[i].url +
-        ' allowfullscreen="allowfullscreen" sandbox="allow-popups allow-forms allow-scripts allow-same-origin" frameborder="0"></iframe>'
-	);
-}
+    for (var i = 0; i < data.length; i++) {
+        div.insertAdjacentHTML(
+            'beforeend',
+            '<iframe style="position:absolute;top:0;left:0;width:10%;height:10%;" src="' +
+            data[i].url +
+            ' allowfullscreen="allowfullscreen" sandbox="allow-popups allow-forms allow-scripts allow-same-origin" frameborder="0"></iframe>'
+        );
+    }
+    */
+})
+
+// Code came from https://www.geeksforgeeks.org/javascript-program-to-read-text-file/
